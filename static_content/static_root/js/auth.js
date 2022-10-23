8f0bbc009965372e5470ad7658f3891b0f735e6d
keyvanmj <keyvanmj5@gmail.com>
2022-10-23 16:27:02 +0330

    push project

--- static_content/static_root/js/auth.js
+++ static_content/static_root/js/auth.js
@@ -1,8 +1,6 @@
 const container = document.getElementById('auth_container_id');
 const login_url = 'account/auth/login/'
 const register_url = 'account/auth/register/'
-console.log(window.location.pathname.includes(login_url))
-console.log(window.location.pathname.includes(register_url))
 if (window.location.pathname.includes(login_url)){
 	container.classList.remove("right-panel-active");
 }
